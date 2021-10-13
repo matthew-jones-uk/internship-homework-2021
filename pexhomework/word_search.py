b89084602bfec32ec6122a60c4a1d15fe508bfb6
Matthew Jones <matthewbtjones2000@gmail.com>
2021-10-13 04:44:44 +0100

    initial working

--- pexhomework/word_search.py
+++ pexhomework/word_search.py
@@ -1,6 +1,23 @@
 class WordSearch:
+    ROW_LENGTH: int = 8
+
     def __init__(self, grid: str) -> None:
         self.grid = grid
 
     def is_present(self, word: str) -> bool:
-        ...
+        for i in range(len(self.grid)):
+            if self.grid[i] == word[0]:
+                # we take the index after the index of the last letter of the word
+                i_end_right = i + len(word)
+                i_end_down = i + ((len(word) - 1) * self.ROW_LENGTH) + 1
+                if (
+                    self.grid[i:i_end_right] == word
+                    and i // self.ROW_LENGTH == (i_end_right - 1) // self.ROW_LENGTH
+                    and i_end_right - 1 < len(self.grid)
+                ):
+                    return True
+                elif self.grid[
+                    i : i_end_down : self.ROW_LENGTH
+                ] == word and i_end_down - 1 < len(self.grid):
+                    return True
+        return False
--- tests/test_pexiphomework.py
+++ tests/test_pexiphomework.py
@@ -1,5 +1,42 @@
 from pexhomework import __version__
+from pexhomework import WordSearch
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.0.1"
+
+
+def test_search_8x8():
+    words_to_find = [
+        "program",
+        "map",
+        "lead",
+        "pleasure",
+        "screen",
+        "metal",
+        "republic",
+    ]
+    grid = "xprogramblxjtpaaleadnfxpfaknfmpalscreenzpulkjteqmrqweatyrepublic"
+
+    ws = WordSearch(grid)
+    for word in words_to_find:
+        assert ws.is_present(word)
+
+
+def test_search_10x10():
+    words_to_find = [
+        "gang",
+        "bangles",
+        "satellite",
+        "grip",
+        "minimum",
+        "guarantee",
+        "bible",
+        "tram",
+    ]
+    grid = "dkfbasdfghpgmajklhbpiutnghgripgangtherbosrplaotylbsatellitemvnsstyrrlkbtasdfjaoieeeminimumdeasdfjklh"
+
+    ws = WordSearch(grid)
+    ws.ROW_LENGTH = 10
+    for word in words_to_find:
+        assert ws.is_present(word)
